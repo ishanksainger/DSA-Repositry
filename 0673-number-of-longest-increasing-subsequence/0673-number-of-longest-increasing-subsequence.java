069db1b988618a51bbf143680c39f214ae3f8979
Ishank Sainger <66199703+ishanksainger@users.noreply.github.com>
2023-07-26 20:20:32 +0530

    Time: 26 ms (47.82%), Space: 43.7 MB (6.65%) - LeetHub

--- 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.java
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.java
@@ -1,37 +1,30 @@
 class Solution {
-    public int findNumberOfLIS(int[] arr) {
-        int n = arr.length;
-    
-    int[] dp= new int[n];
-    int[] ct= new int[n];
-    
-    Arrays.fill(dp,1);
-    Arrays.fill(ct,1);
-    
-    int maxi = 1;
-    
-    for(int i=0; i<=n-1; i++){
-        for(int prev_index = 0; prev_index <=i-1; prev_index ++){
-            
-            if(arr[prev_index]<arr[i] && dp[prev_index]==dp[i]){
-                dp[i] = dp[prev_index]+1;
-                //inherit
-                ct[i] = ct[prev_index];
+    public int findNumberOfLIS(int[] nums) {
+        int n=nums.length;
+        int[] dp=new int[n];
+        int[] count=new int[n];
+        // we are filling it with 1 because all the numbers is already counted and len is already 1 
+        Arrays.fill(dp,1);
+        Arrays.fill(count,1);
+        int max=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i-1;j++){
+                if(nums[i]>nums[j] && dp[j]==dp[i]){
+                    count[i]=count[j];
+                    dp[i]=dp[j]+1;
+                }
+                else if(nums[i]>nums[j] && dp[j]+1==dp[i]){
+                    count[i]+=count[j];
+                }
             }
-            else if(arr[prev_index]<arr[i] && dp[prev_index]+1==dp[i]){
-                //increase the count
-                ct[i] = ct[i] + ct[prev_index];
+            max=Math.max(dp[i],max);
+        }
+        int num=0;
+        for(int i=0;i<n;i++){
+            if(dp[i]==max){
+                num+=count[i];
             }
         }
-         maxi = Math.max(maxi,dp[i]);
-    }
-    
-    int nos =0;
-    
-    for(int i=0; i<=n-1; i++){
-       if(dp[i]==maxi) nos+=ct[i];
-    }
-    
-    return nos;
+        return num;
     }
 }
