150cbd78edc216176584654ab4e842ee8ccb6728
Ishank Sainger <66199703+ishanksainger@users.noreply.github.com>
2023-07-26 14:57:59 +0530

    Time: 251 ms (16.61%), Space: 94.3 MB (19.37%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,26 +1,24 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int n=nums.length;
-        // n+1 because we we are going from -1 to n-1 to it means we needs n number and space of n+1;
-        int[][] dp=new int[n][n+1];
-        for(int[] num: dp){
-            Arrays.fill(num, -1);
-        }
-        // we are sending -1 as prev because starting point is 0
-        return helper(0,-1, nums, dp);
-    }
-    public int helper(int idx, int prev, int[] nums, int[][]dp){
+        //n+1 for both because tabulation its opposite of memoization so we need 0 to n for both;
+        int[][] dp=new int[n+1][n+1];
+       /* no need of base case in tabulation as we are already has array of 0
         if(idx==nums.length){
             return 0;
         }
-        // we are doing prev+1 because -1 is the starting point and index cant be-1 so we are adding +1 in every index so it doesnt give error.
-        if(dp[idx][prev+1]!=-1){
-            return dp[idx][prev+1];
+        */
+        //in both we are starting from n-1 till 0 in index and till -1 for j as it is prev
+        for(int i=n-1;i>-1;i--){
+            for(int j=n-1;j>=-1;j--){
+                int len= 0+ dp[i+1][j+1];
+                if(j==-1 || nums[i]>nums[j]){
+                len= Math.max(len, 1 + dp[i+1][i+1]);
+                }
+                dp[i][j+1]=len;
+            }
         }
-        int len=0+ helper(idx+1, prev, nums,dp);
-        if(prev==-1 || nums[idx]>nums[prev]){
-            len= Math.max(len, 1 + helper(idx+1, idx, nums, dp));
-        }
-        return dp[idx][prev+1]=len;
+        
+        return dp[0][0];
     }
 }
