22faf9d62c113be1fab23852334f05cb5a35ede0
Ishank Sainger <66199703+ishanksainger@users.noreply.github.com>
2023-07-27 09:46:07 +0530

    Time: 276 ms (5.69%), Space: 41.9 MB (22.19%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -4,24 +4,21 @@ class Solution {
         List<Integer> list=Arrays.stream(nums).boxed().collect(Collectors.toList());
         list.add(0,1);
         list.add(1);
-        int[][] dp=new int[n+1][n+1];
-        for(int[] num:dp){
-            Arrays.fill(num,-1);
+        int[][] dp=new int[n+2][n+2];
+        
+        for(int i=n;i>0;i--){
+            for(int j=1;j<=n;j++){
+                if(i>j){
+                    continue;
+                }
+                int max=Integer.MIN_VALUE;
+                for(int k=i;k<=j;k++){
+                    int ans=list.get(i-1) * list.get(k) * list.get(j+1) + dp[i][k-1] + dp[k+1][j];
+                    max=Math.max(max, ans);
+                }
+                dp[i][j]=max;
+            }
         }
-        return helper(dp, 1, n, list);
-    }
-    public int helper(int[][]dp, int i, int j, List<Integer> list){
-        if(i>j){
-            return 0;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        int max=Integer.MIN_VALUE;
-        for(int k=i;k<=j;k++){
-            int ans=list.get(i-1) * list.get(k) * list.get(j+1) + helper(dp, i, k-1, list) + helper(dp, k+1, j, list);
-            max=Math.max(max, ans);
-        }
-        return dp[i][j]=max;
+        return dp[1][n];
     }
 }
