aea9e3950e6f1e3263ec45b1341759b133db96a7
Ishank Sainger <66199703+ishanksainger@users.noreply.github.com>
2023-08-01 21:29:46 +0530

    Time: 1 ms (75.56%), Space: 42 MB (8.01%) - LeetHub

--- 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.java
+++ 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.java
@@ -1,21 +1,19 @@
 class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
         int n=candies.length;
-        PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
+        int max=0;
         List<Boolean> list=new ArrayList<>();
-        for(int i=0;i<n;i++){
-            pq.add(candies[i]);
+        for(int num: candies){
+            max=Math.max(max,num);
         }
         for(int i=0;i<n;i++){
             int num=candies[i]+extraCandies;
-            pq.add(num);
-            if(pq.peek()==num){
+            if(num>=max){
                 list.add(true);
             }
             else{
                 list.add(false);
             }
-            pq.remove(num);
         }
         return list;
     }
