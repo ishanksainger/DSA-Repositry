92d87aa27c6fedc8c8670406bb4180b348e5afb6
Ishank Sainger <66199703+ishanksainger@users.noreply.github.com>
2023-08-08 09:14:03 +0530

    Time: 4 ms (94.17%), Space: 42.8 MB (52.26%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,34 +1,48 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
-        int sum=0;
-        int n=nums.length;
-        for(int num: nums){
-            sum+=num;
+        int sum = 0;
+        int n = nums.length;
+        
+        for (int num : nums) {
+            sum += num;
         }
-        int finalTarget=(sum-target)/2;
-        if(finalTarget<0 || (sum-target)%2!=0){
+        
+        int finalTarget = (sum - target) / 2;
+        
+        if (finalTarget < 0 || (sum - target) % 2 != 0) {
             return 0;
         }
-        int[][] dp=new int[n][finalTarget+1];
-        if(nums[0]==0){
-            dp[0][0]=2;
+        
+        int[][] dp = new int[n][finalTarget + 1];
+        
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
         }
-        else{
-            dp[0][0]=1;
-        }
-        if(nums[0]!=0 && nums[0]<=finalTarget){
-            dp[0][nums[0]]=1;
-        }
-        for(int i=1;i<n;i++){
-            for(int j=0;j<finalTarget+1;j++){
-                int notTake=dp[i-1][j];
-                int take=0;
-                if(nums[i]<=j){
-                    take=dp[i-1][j-nums[i]];
-                }
-                dp[i][j]=notTake+take;
+        
+        return helper(nums, dp, n - 1, finalTarget) % ((int) 1e9 + 7);
+    }
+    
+    public int helper(int[] nums, int[][] dp, int idx, int target) {
+        if (idx == 0) {
+            if (target == 0 && nums[0] == 0) {
+                return 2;
             }
+            if (target == 0 || nums[idx] == target) {
+                return 1;
+            }
+            return 0;
         }
-        return dp[n-1][finalTarget];
+        
+        if (dp[idx][target] != -1) {
+            return dp[idx][target] % ((int) 1e9 + 7);
+        }
+        
+        int notTake = helper(nums, dp, idx - 1, target);
+        int take = 0;
+        if (nums[idx] <= target) {
+            take = helper(nums, dp, idx - 1, target - nums[idx]);
+        }
+        
+        return dp[idx][target] = (notTake + take) % ((int) 1e9 + 7);
     }
 }
