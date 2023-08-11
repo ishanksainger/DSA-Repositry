7da8baa38185050bcc3574fda18117991297388e
Ishank Sainger <66199703+ishanksainger@users.noreply.github.com>
2023-08-11 23:12:40 +0530

    Time: 48 ms (73.9%), Space: 54.8 MB (14.34%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,27 +1,23 @@
 class Solution {
     public int longestPalindromeSubseq(String s) {
-        String text1=s;
-        String text2=new StringBuilder(s).reverse().toString();
-        int n=text1.length();
-        int m=text2.length();
-        int[][]dp=new int[n][m];
-        for(int[] num:dp){
+        String s1=new StringBuilder(s).reverse().toString();
+        int n=s.length();
+        int[][] dp=new int[n][n];
+        for(int[] num: dp){
             Arrays.fill(num,-1);
         }
-        return helper(n-1,m-1,text1,text2, dp);
+        return helper(dp,n-1,n-1,s,s1);
     }
-    public int helper(int idx1, int idx2, String text1, String text2, int[][]dp){
+    public int helper(int[][]dp, int idx1, int idx2, String s1, String s2){
         if(idx1<0 || idx2<0){
             return 0;
         }
         if(dp[idx1][idx2]!=-1){
             return dp[idx1][idx2];
         }
-
-        if(text1.charAt(idx1)==(text2.charAt(idx2))){
-            int max= 1 + helper(idx1-1,idx2-1,text1, text2, dp);
-            return max;
+        if(s1.charAt(idx1)==(s2.charAt(idx2))){
+            return dp[idx1][idx2]=1+helper(dp,idx1-1,idx2-1, s1,s2);
         }
-        return dp[idx1][idx2]=Math.max( helper(idx1-1,idx2,text1, text2, dp),  helper(idx1,idx2-1,text1, text2, dp));
+        return dp[idx1][idx2]=Math.max(helper(dp,idx1-1, idx2, s1,s2),helper(dp, idx1, idx2-1, s1,s2));
     }
 }
