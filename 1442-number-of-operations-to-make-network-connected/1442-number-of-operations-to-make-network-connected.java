ad24e0c2f69de9a18a08449b35555bde2f276b4b
Ishank Sainger <66199703+ishanksainger@users.noreply.github.com>
2023-08-21 08:29:20 +0530

    Time: 19 ms (35.31%), Space: 67 MB (29.45%) - LeetHub

--- 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.java
+++ 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.java
@@ -1,9 +1,9 @@
-class DisJoint{
+class DisJointSet{
     List<Integer> parent=new ArrayList<>();
     List<Integer> rank=new ArrayList<>();
     List<Integer> size=new ArrayList<>();
-    DisJoint(int n){
-        for(int i=0;i<n;i++){
+    DisJointSet(int n){
+        for(int i=0;i<=n;i++){
             parent.add(i);
             rank.add(0);
             size.add(1);
@@ -51,10 +51,9 @@ class DisJoint{
 }
 class Solution {
     public int makeConnected(int n, int[][] connections) {
-        DisJoint ds=new DisJoint(n);
         int extraEdges=0;
-        int m=connections.length;
-        for(int i=0;i<m;i++){
+        DisJointSet ds=new DisJointSet(n);
+        for(int i=0;i<connections.length;i++){
             int u=connections[i][0];
             int v=connections[i][1];
             if(ds.ultParent(u)==ds.ultParent(v)){
@@ -64,17 +63,15 @@ class Solution {
                 ds.bySize(u,v);
             }
         }
-        int ans=0;
         int singleParent=0;
         for(int i=0;i<n;i++){
             if(ds.parent.get(i)==i){
                 singleParent++;
             }
         }
-        ans=singleParent-1;
-        if(extraEdges>=ans){
-            return ans;
+        if(extraEdges>=singleParent-1){
+            return singleParent-1;
         }
-        return -1;
+        return -1;  
     }
 }
