dccf0faa61688b140968a9db888d0716b2cb1959
Ishank Sainger <66199703+ishanksainger@users.noreply.github.com>
2023-08-25 08:10:38 +0530

    Time: 190 ms (26.68%), Space: 67 MB (98.84%) - LeetHub

--- 0854-making-a-large-island/0854-making-a-large-island.java
+++ 0854-making-a-large-island/0854-making-a-large-island.java
@@ -47,58 +47,63 @@ class DisJointSet{
     }
 }
 class Solution {
-    private boolean isValid(int row, int col,int n){
-        return row>=0 && row<n && col>=0 && col<n; 
+    public boolean isValid(int row, int col, int n){
+        return (row>=0 && col>=0 && row<n && col<n );
     }
     public int largestIsland(int[][] grid) {
         int n=grid.length;
+
         DisJointSet ds=new DisJointSet(n*n);
-        
-        //step-1 we will attach all the 1 which is mentioned and set their unionbysize.
+
+        int[] moveRow={-1,0,1,0};
+        int[] moveCol={0,1,0,-1};
 
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
+
                 if(grid[i][j]==0) continue;
-                int[] moveRow={-1,0,1,0};
-                int[] moveCol={0,1,0,-1};
-                for(int k=0;k<4;k++){
-                    int nRow=i+moveRow[k];
-                    int nCol=j+moveCol[k];
-                    if(isValid(nRow,nCol,n) && grid[nRow][nCol]==1){
-                        int nodeNo=i *n + j;
-                        int adjNode=nRow * n + nCol;
-                        ds.unionbySize(nodeNo,adjNode);
-                    }
-                }
-            }
-        }
-        //step 2 in this we will try to set 0 to 1 and check max coverage and save it in hashset
-        int max=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==1) continue;
-                int[] moveRow={-1,0,1,0};
-                int[] moveCol={0,1,0,-1};
-                HashSet<Integer> set=new HashSet<>();
-                for(int k=0;k<4;k++){
-                    int nRow=i+moveRow[k];
-                    int nCol=j+moveCol[k];
-                    if(isValid(nRow,nCol,n)){
-                        if(grid[nRow][nCol]==1){
-                            set.add(ds.ultParent(nRow*n+nCol));
+
+                else{
+                    for(int k=0;k<4;k++){
+                        int nRow=i+moveRow[k];
+                        int nCol=j+moveCol[k];
+                        if(isValid(nRow,nCol,n) && grid[nRow][nCol]==1){
+                            int u=i*n+j;
+                            int v=nRow*n+nCol;
+                            ds.unionbySize(u,v);
                         }
                     }
                 }
-                int sizeTotal=0;
-                for(int parents:set){
-                    sizeTotal+=ds.size.get(parents);
-                }
-                max=Math.max(max,sizeTotal + 1);
             }
-        }
-        for(int i=0;i<n*n;i++){
-            max=Math.max(max, ds.size.get(i));
-        }
-        return max;
+        }       
+        int totalSize=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1) continue;
+                else{
+                    HashSet<Integer> set=new HashSet<>();
+                    for(int k=0;k<4;k++){
+                        int nRow=i+moveRow[k];
+                        int nCol=j+moveCol[k];
+                        if(isValid(nRow,nCol,n) && grid[nRow][nCol]==1){
+                            int v=nRow*n+nCol;
+                            set.add(ds.ultParent(v));
+                        }
+                    }
+                    int size=0;
+                    for(int num:set){
+                        size+=ds.size.get(num);
+                    }
+                    totalSize=Math.max(totalSize,size+1);
+                }
+            }
+        }    
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                int item=i*n+j;
+                totalSize=Math.max(totalSize,ds.size.get(item));
+            }
+        }   
+        return totalSize;
     }
 }
