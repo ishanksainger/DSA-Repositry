fb0ee8707e0ecc8afa1bbe0ac839d46473b69338
Ishank Sainger <66199703+ishanksainger@users.noreply.github.com>
2023-08-28 07:40:47 +0530

    Time: 6 ms (69.73%), Space: 55.1 MB (25.07%) - LeetHub

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.java
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.java
@@ -2,12 +2,13 @@ class Solution {
     public int countSquares(int[][] matrix) {
         int m=matrix.length;
         int n=matrix[0].length;
-        int[][]dp=new int[m][n];
+        int[][] dp=new int[m][n];
+
+        for(int i=0;i<m;i++){
+            dp[i][0]=matrix[i][0];
+        }
         for(int i=0;i<n;i++){
             dp[0][i]=matrix[0][i];
-        }
-         for(int i=0;i<m;i++){
-            dp[i][0]=matrix[i][0];
         }
         for(int i=1;i<m;i++){
             for(int j=1;j<n;j++){
@@ -15,19 +16,16 @@ class Solution {
                     dp[i][j]=0;
                 }
                 else{
-                    int left=dp[i][j-1];
-                    int up=dp[i-1][j];
-                    int upLeft=dp[i-1][j-1];
-                    dp[i][j]=1 + Math.min(left, Math.min(upLeft, up));
+                    dp[i][j]=Math.min(dp[i-1][j],Math.min(dp[i-1][j-1],dp[i][j-1]))+1;
                 }
             }
         }
-        int totalSum=0;
-        for(int i=0;i<m;i++){
+        int sum=0;
+         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                totalSum+=dp[i][j];
+                sum+=dp[i][j];
             }
         }
-        return totalSum;
+        return sum;
     }
 }
