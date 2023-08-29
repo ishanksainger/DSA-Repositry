83084be97789a19e762f8e57d0b6f578d04d5bfa
Ishank Sainger <66199703+ishanksainger@users.noreply.github.com>
2023-08-29 20:48:52 +0530

    Time: 37 ms (15.71%), Space: 41.7 MB (35.48%) - LeetHub

--- 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.java
+++ 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.java
@@ -1,31 +1,27 @@
 class Solution {
     public int minCost(int n, int[] cuts) {
-        // we convert the cuts array to list because we need to include 0 and n in the cuts array.
+        int size=cuts.length;
         List<Integer> list=Arrays.stream(cuts).boxed().collect(Collectors.toList());
         list.add(0,0);
         list.add(n);
-        // we sort the list so that we can cut the stick in a normaly way and sequntial way.
         Collections.sort(list);
-        //orignal cuts length
-        int c=cuts.length;
-        // for tabulation we are increasing the size
-        int[][] dp=new int[c+2][c+2];
-        // in tabulation we do opposite of recursion so we start from c instead of in i and in j we start from 1 instead
-        //of c
-        for(int i=c;i>=1;i--){
-            for(int j=1;j<=c;j++){
-                if(i>j){
-                    continue;
-                }
-                // this below function will remain same;
-                int min=Integer.MAX_VALUE;
-                for(int k=i;k<=j;k++){
-                    int ans=list.get(j+1)-list.get(i-1) + dp[i][k-1] + dp[k+1][j];
-                    min=Math.min(min,ans);
-                }
-                dp[i][j]=min;
-            }
+        int[][] dp=new int[size+1][size+1];
+        for(int[] num: dp){
+            Arrays.fill(num,-1);
         }
-        return dp[1][c];
+        return helper(1,size,dp, list);
+    }
+    public int helper(int start,int n, int[][] dp, List<Integer> list){
+        if(start>n){
+            return 0;
+        }
+        if(dp[start][n]!=-1){
+            return dp[start][n];
+        }
+        int min=Integer.MAX_VALUE;
+        for(int i=start;i<=n;i++){
+            min=Math.min(min, list.get(n+1)-list.get(start-1) + helper(start,i-1,dp,list)+ helper(i+1,n,dp,list) );
+        }
+        return dp[start][n]=min;
     }
 }
