2c64afe2ab35a39bceb2ab354dfbc62d3cc439c6
Ishank Sainger <66199703+ishanksainger@users.noreply.github.com>
2023-09-08 22:29:32 +0530

    Time: 0 ms (100%), Space: 41 MB (46.45%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,22 +1,13 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
         int n=nums.length;
-        int count=0;
+        int index=0;
         for(int i=0;i<n;i++){
-            if(val==nums[i]){
-                count++;
-                nums[i]=-100;
+            if(nums[i]!=val){
+                nums[index]=nums[i];
+                index++;
             }
         }
-        List<Integer> list=Arrays.stream(nums).boxed().collect(Collectors.toList());
-        list.removeAll(java.util.Collections.singleton(-100));
-        int size=list.size();
-        for(int i=0;i<count;i++){
-            list.add(-100);
-        }
-        for(int i=0;i<n;i++){
-            nums[i]=list.get(i);
-        }
-        return size;
+        return index;
     }
 }
