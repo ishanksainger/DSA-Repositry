219de288f276111ba6cc8fcfb68784811937c44d
Ishank Sainger <66199703+ishanksainger@users.noreply.github.com>
2023-09-19 09:18:29 +0530

    Time: 1423 ms (5%), Space: 43.5 MB (81.76%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int n=ratings.length;
+        int[] candies=new int[n];
+        Arrays.fill(candies,1);
+        for(int i=0;i<n;i++){
+     
+          if(i>0 && i<n-1 && ratings[i]>ratings[i-1] && ratings[i]>ratings[i+1]){
+            candies[i]=candies[i-1]+1;
+          }
+          else if(i!=0 && ratings[i]>ratings[i-1]){
+            candies[i]=candies[i-1]+1;
+          }
+          else if(i!=n-1 && ratings[i]>ratings[i+1]){
+            candies[i]=candies[i+1]+1;
+            while(i>0 && candies[i]==candies[i-1] && ratings[i]<ratings[i-1]){
+              candies[i-1]++;
+              i--;
+            }
+          }
+        }
+        int sum=0;
+        for(int i=0;i<n;i++){
+          // System.out.println(candies[i]);
+          sum+=candies[i];
+        }
+        return sum;
+    }
+}
