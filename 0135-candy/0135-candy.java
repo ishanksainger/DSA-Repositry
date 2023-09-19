6f0cf96331ff7eab01cfe2a7dad4a7ad3264de78
Ishank Sainger <66199703+ishanksainger@users.noreply.github.com>
2023-09-19 09:46:24 +0530

    Time: 2 ms (99.92%), Space: 43.5 MB (70.59%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -2,26 +2,22 @@ class Solution {
     public int candy(int[] ratings) {
         int n=ratings.length;
         int[] candies=new int[n];
-        for(int i=0;i<n;i++){
-          if((i>0 && i<n-1 && ratings[i]>ratings[i-1] && ratings[i]>ratings[i+1]) || (i!=0 && ratings[i]>ratings[i-1])){
+
+        for(int i=1;i<n;i++){
+          if(ratings[i]>ratings[i-1]){
             candies[i]=candies[i-1]+1;
           }
-          else if(i!=n-1 && ratings[i]>ratings[i+1]){
-            candies[i]=candies[i+1]+1;
-            helper(ratings,i,candies);
+        }
+         for(int i=n-2;i>-1;i--){
+          if(ratings[i]>ratings[i+1]){
+            candies[i]=Math.max(candies[i],candies[i+1]+1);
           }
         }
+        
         int sum=0;
         for(int i=0;i<n;i++){
           sum+=candies[i]+1;
         }
         return sum;
-    }
-    public void helper(int[]ratings,int i, int[]candies){
-       while(i>0 && candies[i]==candies[i-1] && ratings[i]<ratings[i-1]){
-              candies[i-1]++;
-              i--;
-      }
-
     }
 }
