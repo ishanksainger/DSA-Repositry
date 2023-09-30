8b9c189389c750b8de2e2d65b88d570ed08d5b1f
Ishank Sainger <66199703+ishanksainger@users.noreply.github.com>
2023-09-30 09:58:57 +0530

    Time: 1 ms (100%), Space: 42.9 MB (84.27%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,13 +1,14 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        boolean[][] rows = new boolean[9][9];
-        boolean[][] columns = new boolean[9][9];
-        boolean[][] boxes = new boolean[9][9];
+      // we aremaking this booleans
+        boolean[][] rows = new boolean[9][10];
+        boolean[][] columns = new boolean[9][10];
+        boolean[][] boxes = new boolean[9][10];
 
         for (int row = 0; row < 9; row++) {
             for (int col = 0; col < 9; col++) {
                 if (board[row][col] != '.') {
-                    int num = board[row][col] - '1';
+                    int num = board[row][col] - '0';
                     int boxIndex = (row / 3) * 3 + col / 3;
 
                     if (rows[row][num] || columns[col][num] || boxes[boxIndex][num]) {
