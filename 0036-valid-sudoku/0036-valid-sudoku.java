86c637fbba8f9b44b0541530a4ded7df994aebb6
Ishank Sainger <66199703+ishanksainger@users.noreply.github.com>
2023-09-30 10:16:54 +0530

    Time: 1 ms (100%), Space: 43.3 MB (55.5%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,27 +1,29 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-      // we aremaking this booleans
+      // we are making this booleans to keep track of the number that we have already visited. and we mark them as true.
         boolean[][] rows = new boolean[9][10];
         boolean[][] columns = new boolean[9][10];
         boolean[][] boxes = new boolean[9][10];
-
+        // now we visit each row and column
         for (int row = 0; row < 9; row++) {
             for (int col = 0; col < 9; col++) {
                 if (board[row][col] != '.') {
+                  // here we get the board number of that particular row and column
                     int num = board[row][col] - '0';
+                    // this formula is for 3 x 3 box as we have to check if the same number is in the box or not.
                     int boxIndex = (row / 3) * 3 + col / 3;
-
+                    // this condiiton will checvk if any of the number is already true or you can say already visited. so we return false
                     if (rows[row][num] || columns[col][num] || boxes[boxIndex][num]) {
                         return false;
                     }
-
+                    // else you keep marking the visiteed number as true in row colum and 3 x 3 matrix
                     rows[row][num] = true;
                     columns[col][num] = true;
                     boxes[boxIndex][num] = true;
                 }
             }
         }
-
+      // if none of them are visited then we return true
         return true;
     }
 }
