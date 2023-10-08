b55e813be01556064638a18076282b37606c33d2
Ishank Sainger <66199703+ishanksainger@users.noreply.github.com>
2023-10-08 10:27:17 +0530

    Time: 28 ms (57.84%), Space: 58.1 MB (46.82%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -4,14 +4,14 @@ class Solution {
         Set<Integer> set=Arrays.stream(nums).boxed().collect(Collectors.toSet());
         int max=0;
         for(int i=0;i<n;i++){
-          if(!set.contains(nums[i]-1)){
+            if(!set.contains(nums[i]-1)){
               int currNum=nums[i];
               int count=0;
-         
             while(set.contains(currNum++)){
               count++;
             } 
             max=Math.max(max,count);
+            if(max>n/2) break;
            }
         }
         return max;
