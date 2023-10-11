d6b7ba832f4778a11cd7ab1c60fbc90548826bca
Ishank Sainger <66199703+ishanksainger@users.noreply.github.com>
2023-10-11 12:19:27 +0530

    Time: 16 ms (22.94%), Space: 43.9 MB (64.69%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int calculate(String s) {
+        s=s.replaceAll("\\s","");
+        Stack<Integer> stack=new Stack<>();
+         int sum=0,sign=1;
+        for(int i=0;i<s.length();i++){
+
+            if(s.charAt(i)>='0' && s.charAt(i)<='9'){
+               int num=0;
+               while(s.length() > i && s.charAt(i)>='0' && s.charAt(i)<='9'){
+                   num=num*10+(s.charAt(i)-'0');
+                   i++;
+                }
+                sum+=num*sign;
+                i--;
+            }
+            else if(s.charAt(i)=='+'){
+                sign=1;
+            }
+            else if(s.charAt(i)=='-'){
+                sign=-1;
+            }
+            else if(s.charAt(i)=='('){
+                stack.push(sum);
+                stack.push(sign);
+                sign=1;
+                sum=0;
+            }
+            else if(s.charAt(i)==')'){
+               sum=sum*stack.pop();
+               sum+=stack.pop();
+            }
+        }
+        return sum;        
+    }
+}
