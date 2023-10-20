862b0c53bfcbac533d9374fdf155f1db8eae356d
Ishank Sainger <66199703+ishanksainger@users.noreply.github.com>
2023-10-21 00:37:42 +0530

    Time: 1 ms (96.12%), Space: 43 MB (81.14%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -1,23 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    private int i = 0;
-    private int p = 0;
-
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return build(preorder, inorder, Integer.MIN_VALUE);
+        Map<Integer,Integer> map=new HashMap<>();
+        int n=inorder.length;
+        int m=preorder.length;
+        for(int i=0;i<n;i++){
+            map.put(inorder[i],i);
+        }
+       return createTree(preorder,0,m-1,inorder,0,n-1,map);
     }
-
-    private TreeNode build(int[] preorder, int[] inorder, int stop) {
-        if (p >= preorder.length) {
+    public TreeNode createTree(int[] preorder,int preStart,int preEnd, int[] inorder, int inStart, int inEnd, Map<Integer,Integer> map){
+        if(preStart>preEnd || inStart > inEnd){
             return null;
         }
-        if (inorder[i] == stop) {
-            ++i;
-            return null;
-        }
+        TreeNode root=new TreeNode(preorder[preStart]);
+        int inRoot=map.get(root.val);
+        int numsLeft=inRoot-inStart;
 
-        TreeNode node = new TreeNode(preorder[p++]);
-        node.left = build(preorder, inorder, node.val);
-        node.right = build(preorder, inorder, stop);
-        return node;
+        root.left=createTree(preorder,preStart+1, preStart+numsLeft,inorder, inStart, inRoot-1,map);
+        root.right=createTree(preorder,preStart+numsLeft+1,preEnd,inorder,inRoot+1, inEnd,map);
+        return root;
     }
 }
