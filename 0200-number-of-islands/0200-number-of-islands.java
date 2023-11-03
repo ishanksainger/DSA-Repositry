f7d79ffb7b4ff761616dfd58df87052d0d8cf288
Ishank Sainger <66199703+ishanksainger@users.noreply.github.com>
2023-11-03 10:01:12 +0530

    Time: 6 ms (23.15%), Space: 50.3 MB (17.55%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,45 +1,43 @@
 class Solution {
     class Edge{
-        int row,col;
+        int row, col;
         Edge(int row, int col){
             this.row=row;
             this.col=col;
         }
     }
-    public void bfs(int row, int col,char[][] grid, boolean[][] visited){
-        Queue<Edge> queue=new LinkedList<>();
-        int n=grid.length;
-        int m=grid[0].length;
-        queue.add(new Edge(row,col));
-        int[] moveRow={-1,0,1,0};
-        int[] moveCol={0,1,0,-1};
-        while(!queue.isEmpty()){
-            Edge e=queue.remove();
-            int r=e.row;
-            int c=e.col;
-            for(int i=0;i<4;i++){
-                int nRow=r+moveRow[i];
-                int nCol=c+moveCol[i];
-                if(nRow>=0 && nRow<n && nCol>=0 && nCol<m && !visited[nRow][nCol] && grid[nRow][nCol]=='1'){
-                    queue.add(new Edge(nRow,nCol));
-                    visited[nRow][nCol]=true;
-                }
-            }
-        }
-    }
     public int numIslands(char[][] grid) {
-        int n=grid.length;
-        int m=grid[0].length;
-        boolean[][] visited=new boolean[n][m];
         int count=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(!visited[i][j] && grid[i][j]=='1'){
-                    bfs(i,j,grid,visited);
+        int m=grid.length;
+        int n=grid[0].length;
+        int[][] visited=new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(visited[i][j]==0 && grid[i][j]=='1'){
                     count++;
+                    bfs(grid, visited, m,n, i, j);
                 }
             }
         }
         return count;
+    }
+    public void bfs(char[][] grid, int[][] visited, int m, int n, int ro, int co){
+        Queue<Edge> queue=new LinkedList<>();
+        queue.add(new Edge(ro,co));
+        int[] moveRow={-1,0,1,0};
+        int[] moveCol={0,1,0,-1};
+        while(!queue.isEmpty()){
+            int r=queue.peek().row;
+            int c=queue.peek().col;
+            queue.remove();
+            for(int i=0;i<4;i++){
+                int nextRow=r+moveRow[i];
+                int nextCol=c+moveCol[i];
+                if(nextRow>=0 && nextRow<m && nextCol>=0 && nextCol<n && visited[nextRow][nextCol]==0 && grid[nextRow][nextCol]=='1'){
+                    visited[nextRow][nextCol]=1;
+                    queue.add(new Edge(nextRow, nextCol));
+                }
+            }
+        }
     }
 }
