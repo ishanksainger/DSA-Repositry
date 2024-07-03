c82f7f3e19d6fc3807716373ab38d60f65a7429b
Ishank Sainger <66199703+ishanksainger@users.noreply.github.com>
2024-07-03 17:53:28 +0530

    Time: 1 ms (81.13%), Space: 43.4 MB (47.03%) - LeetHub

--- 0541-reverse-string-ii/0541-reverse-string-ii.java
+++ 0541-reverse-string-ii/0541-reverse-string-ii.java
@@ -5,6 +5,10 @@ class Solution {
         for (int i = 0; i < s.length() - 1; i += 2 * k) {
             int start = i;
             int end = Math.min(i + k - 1, s.length() - 1);
+            // All three conditions match
+            //1 . we move by 2k and get the end and then do while and reverse the k
+            //2. if n less than k then minimum will be length so it will reverse string characters
+            //3. if less than 2k then it will reverse first and when it will increment 2k then it will exit
             
             while (start < end) {
                 char temp = chars[start];
