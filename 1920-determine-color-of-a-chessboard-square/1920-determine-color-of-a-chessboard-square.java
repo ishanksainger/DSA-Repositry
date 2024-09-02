ab2ac4088d7118f5c7c8086d2a20f2a84bf24a5f
Ishank Sainger <66199703+ishanksainger@users.noreply.github.com>
2024-09-02 07:43:20 +0530

    Time: 0 ms (100%), Space: 41 MB (82.45%) - LeetHub

--- 1920-determine-color-of-a-chessboard-square/1920-determine-color-of-a-chessboard-square.java
+++ 1920-determine-color-of-a-chessboard-square/1920-determine-color-of-a-chessboard-square.java
@@ -2,7 +2,6 @@ class Solution {
     public boolean squareIsWhite(String coordinates) {
         int num1 = coordinates.charAt(0) - 'a';
         int num2 = coordinates.charAt(1);
-        System.out.println(num1);
         return (num1 + 1 + num2) % 2 == 0 ? false : true;
     }
 }
